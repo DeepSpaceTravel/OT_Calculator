17c978ae86b9695c0370d3d18ea72e9ab44c6d04
DeepSpaceTravel <141806005+DeepSpaceTravel@users.noreply.github.com>
2025-01-20 11:06:14 +0800

    Refactor: Removed Voyager Framework
    Ready to convert to native Navigation Framework

---
+++ composeApp/src/commonMain/kotlin/ui/EntryListViewModel.kt
@@ -0,0 +1,13 @@
+package ui
+
+import cafe.adriel.voyager.core.model.ScreenModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class EntryListScreenModel: ScreenModel {
+
+//    private val _uiState = MutableStateFlow(EntryListUiState())
+//    val uiState: StateFlow<EntryListUiState> = _uiState.asStateFlow()
+
+}
